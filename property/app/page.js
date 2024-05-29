914ca34e864ed638618d086e2f8e870b7a3ef860
sagar <sagar201993>
2024-05-29 14:05:48 +0200

    add home page properties

--- property/app/page.js
+++ property/app/page.js
@@ -1,12 +1,15 @@
 import Hero from "@/components/Hero";
+import HomeProperties from "@/components/HomeProperties";
 import InfoBoxes from "@/components/InfoBoxes";
 import React from "react";
+import properites from "@/properties.json";
 
 const page = () => {
   return (
     <>
       <Hero />
       <InfoBoxes />
+      <HomeProperties />
     </>
   );
 };
