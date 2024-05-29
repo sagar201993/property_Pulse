0d7c200d99c094a99705ce364c81e1c4fddfb5f6
sagar <sagar201993>
2024-05-29 16:47:11 +0200

    first api route from database

--- property/app/properties/page.jsx
+++ property/app/properties/page.jsx
@@ -1,8 +1,21 @@
 import React from "react";
-import properties from "@/properties.json";
+//import properties from "@/properties.json";
 import PropertyCard from "@/components/PropertyCard";
 
-const PropertiesPage = () => {
+async function fetchProperties() {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`);
+
+    if (!res.ok) {
+      throw new Error("failed to fetch data");
+    }
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
+const PropertiesPage = async () => {
+  const properties = await fetchProperties();
   return (
     <>
       <section className="px-4 py-6">
