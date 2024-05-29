7e8de67d3b4092f827a190317702cf14633030ff
sagar <sagar201993>
2024-05-29 16:50:37 +0200

    sorted propeties according to date

--- property/app/properties/page.jsx
+++ property/app/properties/page.jsx
@@ -16,6 +16,7 @@ async function fetchProperties() {
 }
 const PropertiesPage = async () => {
   const properties = await fetchProperties();
+  properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
   return (
     <>
       <section className="px-4 py-6">
