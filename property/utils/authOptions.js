7bfce3c73bce4112f850441ae18a818e2b0defbf
sagar <sagar201993>
2024-05-30 13:49:35 +0200

    implemented auth options and route file from next auth

---
+++ property/utils/authOptions.js
@@ -0,0 +1,26 @@
+import GoogleProvider from "next-auth/providers/google";
+
+const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    //invoked on signin
+    async signIn({ profile }) {},
+  },
+
+  //modify the seesion object
+  async session({ session }) {},
+};
+
+export default authOptions;
